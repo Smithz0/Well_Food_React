367534a7d9ec625c0d2296a60db79f0e971a7928
Smithz0 <smithrajr@gmail.com>
2025-10-25 14:22:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+
+const App = () => {
+  return (
+    <div>
+      <div className='app'>
+        <Navbar/>
+      </div>
+    </div>
+  )
+}
+
+export default App
